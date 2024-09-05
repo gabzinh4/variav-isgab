c6bfe569049e93dae33aa522e07e032eb5b9cf10
Gaby Rodrigues <127845196+gabzinh4@users.noreply.github.com>
2024-09-05 01:15:06 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let anoAtual = 2024;
+const nomeCompleto = "Gabriely Souza"
+let idade = 18;
+const cursoFaculdade = "administração";
+let cidadeAtual = "SJP";
+
+console.log( `olá, eu sou a ${nomeCompleto}
+eu tenho ${idade} anos, moro em ${cidadeatual} quero ano que vem estar cursando ${cursofaculdade} ` );
+
+anoAtual = 2025;
+
+console.log(`Para meu ano que vem, meu nome ainda será ${nomeCompleto} `); 
+
